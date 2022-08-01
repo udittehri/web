ac4e06cf54c0ef52717c709032a50b487b3f49de
Udit <udittehri@icloud.com>
2022-08-01 18:41:49 +0530

    model changes

--- models/Profile.js
+++ models/Profile.js
@@ -4,16 +4,45 @@ var schema = new mongoose.Schema({
 
     name: {
         type: String,
-        required: true
-    },
-
-    class: {
-        type: Number,
         required: false
-    }
+    },
+    city: {
+        type: String,
+        required: false
+    },
+    describes: {
+        type: String,
+        required: false
+    },
+    discord: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: false
+    },
+    interests: {
+        type: String,
+        required: false
+    },
+    level: {
+        type: String,
+        required: false
+    },
+    reason: {
+        type: String,
+        required: false
+    },
+    twitter: {
+        type: String,
+        required: false
+    },
 
 }, {
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
 });
 
 module.exports = mongoose.model('Profiles', schema);
+
+
